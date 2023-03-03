20e8d128232577fcf0308bc2d247381e5bae97e8
Vishwa Shankar <vishwas1020@gmail.com>
2023-03-03 01:10:55 -0800

    typescript install

--- vc-frontend/vue.config.js
+++ vc-frontend/vue.config.js
@@ -1,7 +1,21 @@
+const path = require('path')
 module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  chainWebpack: config => {
+    config
+      .entry('app')
+      .clear()
+      .add('./src/main.ts')
+      .end();
+    config.module.rule('ts')
+      .test(/\.ts$/)
+      .use('ts-loader')
+      .loader('ts-loader')
+      .options({appendTsSuffixTo: [/\.vue$/]});
+  },
+  
   devServer: {
 
     host: '0.0.0.0'
