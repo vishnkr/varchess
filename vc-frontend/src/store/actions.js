a495bd7b1f24c3ac9fbb1b8e87da14385a93474f
Vishwa Shankar <vishwas1020@gmail.com>
2023-05-20 12:50:32 -0700

    remove old

--- vc-frontend/src/store/actions.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const actions ={
-    async getPossibleToSquares(state,payload){
-        let possibleMoves = await axios.post(`${process.env.VUE_APP_SERVER_HOST}/getPossibleToSquares`,JSON.stringify(payload));
-        return possibleMoves.data;
-    }
-}
-export default actions;
--- vc-frontend/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  devServer: {
-
-    host: '0.0.0.0'
-    }
-}
