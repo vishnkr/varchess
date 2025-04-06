9503e16c2dc89631e36aab70d09964dffee32526
Vishwa Shankar <vishwas1020@gmail.com>
2025-04-07 01:13:42 +0530

    remove wasmpack

--- vc-client/vite.config.ts
+++ vc-client/vite.config.ts
@@ -1,9 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import wasmPack from 'vite-plugin-wasm-pack'
+//import wasmPack from 'vite-plugin-wasm-pack'
 
 export default defineConfig({
-	plugins: [sveltekit(), wasmPack('./stonkfish')],
+	plugins: [sveltekit()], //wasmPack('./stonkfish')],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
